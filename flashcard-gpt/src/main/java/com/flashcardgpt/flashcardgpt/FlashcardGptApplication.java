76bec3411634f38f7024cd5dac7ed52e3f87e5f6
ananyasingh7 <shtoogienet@gmail.com>
2023-07-11 21:46:41 -0400

    initial commit

---
+++ flashcard-gpt/src/main/java/com/flashcardgpt/flashcardgpt/FlashcardGptApplication.java
@@ -0,0 +1,13 @@
+package com.flashcardgpt.flashcardgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardGptApplication.class, args);
+	}
+
+}
---
+++ flashcard-gpt/src/test/java/com/flashcardgpt/flashcardgpt/FlashcardGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flashcardgpt.flashcardgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
